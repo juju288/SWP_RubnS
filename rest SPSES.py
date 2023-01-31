2b484920adce17e81300cb92383ccd20b010a7ad
juju288 <62132596+juju288@users.noreply.github.com>
2023-01-31 14:20:25 +0100

    Update rest SPSES.py

--- rest SPSES.py
+++ rest SPSES.py
@@ -1,28 +1,38 @@
+import flask
 from flask import Flask, request
 from flask_restful import Resource, Api
-
 
 app = Flask(__name__)
 api = Api(app)
 
 nameScore = {}
 
-class Statistik(Resource):
-    def get(self, name):
-        if name in nameScore:
-            return {name: nameScore[name]}
-        return {"Message" : "Nicht vorhanden"}
 
-    def post(self, name):
-        print(name)
-        existing = name in nameScore
-        d = request.get_json(force=True)
-        nameScore[name] = {'playerWins': d['playerWins'], 'compWins':d['compWins'], 'stats': d['stats']}
-        if existing:
-            return {"Message" : "Überschrieben"}
-        return {"Message" : "Neu hinzugefügt"}
+@app.route("/")
+def hello_world():
+    return "hello world"
 
-api.add_resource(Statistik, '/stats/<string:name>')
+
+@app.route("/get", methods=['GET', 'POST'])
+def get():
+    r = request.get_json(force=True)
+    if r["name"] in list(nameScore.keys()):
+        return {r["name"]: nameScore[r["name"]]}
+    #return flask.jsonify(nameScore)
+    return {"Message": "Nicht vorhanden"}
+
+
+@app.route("/post", methods=['POST'])
+def put():
+    r = request.get_json(force=True)
+    print(r["name"])
+    existing = r["name"] in list(nameScore.keys())
+    d = r["statistic"]
+    nameScore[r["name"]] = {'playerWins': d['Players-Wins'], 'compWins': d['Computers-Wins'], 'draw': d['Draw']}
+    if existing:
+        return flask.jsonify({"Message": "Überschrieben"})
+    return flask.jsonify({"Message": "Neu hinzugefügt"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
