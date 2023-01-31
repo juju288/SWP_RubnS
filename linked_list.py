e3b24dcb0fb9c4ddae08e5eb9b9c414c445499b4
juju288 <62132596+juju288@users.noreply.github.com>
2023-01-31 11:46:13 +0100

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env /usr/bin/python3
 import random
 
 
@@ -7,6 +6,8 @@ class Node:
         self.data = data
         self.next = next
 
+        
+        
 
 class LinkedList:
     def __init__(self, data=None):
@@ -17,6 +18,8 @@ class LinkedList:
                 n.next = Node(d)
                 n = n.next
 
+                
+                
     def __str__(self):
         node = self.head
         out = ""
@@ -26,6 +29,7 @@ class LinkedList:
         out += str(node.data)
         return out
 
+    
     def __len__(self):
         if self.head is None:
             return 0
@@ -36,12 +40,11 @@ class LinkedList:
             n = n.next
         return cnt
 
+    
+    
     def insert(self, i, data):
         if i <= 0:
             self.head = Node(data, self.head)
-            # new = Node(data)
-            # new.next = self.head
-            # self.head = new
             return
 
         if i >= len(self):
@@ -53,9 +56,8 @@ class LinkedList:
             n = n.next
             i -= 1
         n.next = Node(data, n.next)
-        # newnode = Node(data)
-        # newnode.next = n.next
-        # n.next = newnode
+
+        
 
     def append(self, data):
         n = self.head
@@ -64,8 +66,9 @@ class LinkedList:
         n.next = Node(data)
 
 
+        
 if __name__ == "__main__":
-    lst = LinkedList([random.randint(0, 9) for i in range(0, random.randint(5, 20))])
+    lst = LinkedList([random.randint(0, 9) for i in range(0, random.randint(4, 18))])
     print(lst)
     lst.insert(-1, 3)
     print(lst)
