d1f727b0e600adb0ba1f0fc3624dff0099ef47ad
juju288 <62132596+juju288@users.noreply.github.com>
2023-03-28 18:39:25 +0200

    Update SchwarzWeissDruck.java

--- ProxyDrucker/src/SchwarzWeissDruck.java
+++ ProxyDrucker/src/SchwarzWeissDruck.java
@@ -1,8 +1,8 @@
 public class SchwarzWeissDruck implements Drucker{
 
     @Override
-    public void drucken(String document) {
-        System.out.println("Drucke in Schwarz und Weiss: " + document);
+    public void drucken(String text) {
+        System.out.println("Drucke in Schwarz und Weiss: " + text);
 
     }
 }
