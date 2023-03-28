ced1949423d7ce02431ab1aa522599a69ba6039d
juju288 <62132596+juju288@users.noreply.github.com>
2023-03-28 18:38:59 +0200

    Update FarbDruck.java

--- ProxyDrucker/src/FarbDruck.java
+++ ProxyDrucker/src/FarbDruck.java
@@ -1,8 +1,8 @@
 public class FarbDruck implements Drucker {
 
     @Override
-    public void drucken(String document) {
-        System.out.println("Drucke mit Farbe: " + document);
+    public void drucken(String text) {
+        System.out.println("Drucke mit Farbe: " + text);
 
     }
 }
